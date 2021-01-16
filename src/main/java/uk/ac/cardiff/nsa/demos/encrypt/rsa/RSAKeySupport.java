8b41b71062ff61d707a1790e527a3d98805e9798
Philip Smart <philip.smart@jisc.ac.uk>
2021-01-16 10:56:27 +0000

    parallel

--- src/main/java/uk/ac/cardiff/nsa/demos/encrypt/rsa/RSAKeySupport.java
+++ src/main/java/uk/ac/cardiff/nsa/demos/encrypt/rsa/RSAKeySupport.java
@@ -3,11 +3,13 @@ package uk.ac.cardiff.nsa.demos.encrypt.rsa;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class RSAKeySupport {
 
     public static BigInteger generatePrime() {
-         for (long i=1000000000; i < 1445005645; i ++) {
+         for (int i=1000000000; i < 1445005645; i ++) {
              if (isPrime(i)) {
                  return BigInteger.valueOf(i);
              }
@@ -16,17 +18,28 @@ public class RSAKeySupport {
     }
 
     // checks whether an int is prime or not.
-    public static boolean isPrime(long n) {
-        for (long i = 2; 2 * i < n; i++) {
-            if (n % i == 0)
-                return false;
+    public static boolean isPrime(int n) {
+        long current = System.currentTimeMillis();
+        if (n%2==0) {
+            return false;
         }
-        return true;
+        OptionalInt prime = IntStream.rangeClosed(3, n-1).parallel()
+                .peek(p -> {if (p%100000000==0) System.out.println("checking:"+p);})
+                .filter(p -> n%p==0)
+                .peek(p -> System.out.println("not prime:"+p))
+                .findAny();
+
+        long end = System.currentTimeMillis();
+        System.out.println("Divides by: "+prime+" time: "+(end-current)/1000);
+        return !prime.isPresent();
     }
     
     public static boolean isPrime(BigInteger n) {
+        long baseTime = System.currentTimeMillis();
         for (BigInteger i = BigInteger.valueOf(2); i.compareTo(n) < 0;i = i.add(BigInteger.ONE)) {
-            //System.out.println(i);
+            if (i.mod(BigInteger.valueOf(1000000)).equals(BigInteger.ZERO)) {
+                System.out.println((System.currentTimeMillis()-baseTime)+": "+i);
+            } 
             if (n.mod(i).equals(BigInteger.ZERO))
                 return false;
         }
@@ -39,11 +52,16 @@ public class RSAKeySupport {
     
     public static void main(String args[]) {
         BigInteger primeOne = BigInteger.probablePrime(1024, new SecureRandom());
+        
+       // IntStream.rangeClosed(1, 150).parallel().forEach(i->System.out.println(i));
+        
+        System.out.println("isPrime: "+RSAKeySupport.isPrime(2147483647));
        System.out.println(primeOne.bitLength());
+      
       
        System.out.println("Rnd int: "+new SecureRandom().nextLong());
        System.out.println("What bit length is this: "+BigInteger.valueOf(60).bitLength());
-       System.out.println(RSAKeySupport.isPrime(primeOne));
+      // System.out.println(RSAKeySupport.isPrime(primeOne));
         //System.out.println(RSAKeySupport.generatePrime());
     }
 
